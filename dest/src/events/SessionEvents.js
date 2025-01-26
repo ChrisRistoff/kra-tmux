3d854843163946c0d374efe16a04d2c9d9fb9c2a
ChrisRistoff <doanusho@gmail.com>
2025-01-26 21:11:19 +0000

    update ignore and remove dest

--- dest/src/events/SessionEvents.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.SessionEvent = void 0;
-var SessionEvent;
-(function (SessionEvent) {
-    SessionEvent["OnSessionsRequest"] = "OnSessionRequest";
-})(SessionEvent || (exports.SessionEvent = SessionEvent = {}));
