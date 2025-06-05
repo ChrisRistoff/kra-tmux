0692521ef7de3e34876535cdb34a9ae7d89153d5
ChrisRistoff <doanusho@gmail.com>
2025-06-05 11:48:41 +0100

    create-new-branch
    checks out on chosen branch, fetches prune and hard resets, creates a
    new branch and checks out on it.

--- auto.sh
+++ auto.sh
@@ -14,7 +14,7 @@ _kra_completions() {
 
     # Second-level commands after "git" or "tmux"
     if [[ "$prev" == "git" ]]; then
-        COMPREPLY=( $(compgen -W "open-pr view-changed restore cache-untracked retrieve-untracked hard-reset log stash stash-drop-multiple conflict-handle" -- "$cur") )
+        COMPREPLY=( $(compgen -W "create-branch open-pr view-changed restore cache-untracked retrieve-untracked hard-reset log stash stash-drop-multiple conflict-handle" -- "$cur") )
     elif [[ "$prev" == "tmux" ]]; then
         COMPREPLY=( $(compgen -W "save-server load-server list-sessions delete-server kill" -- "$cur") )
     elif [[ "$prev" == "sys" ]]; then
