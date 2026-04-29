9d65bb06ad7a48c51df3b52e50584e3eeb591d42
ChrisRistoff <doanusho@gmail.com>
2026-04-29 19:27:33 +0100

    chore: remove remaining watch options, add more docs to crawl

--- src/types/settingsTypes.ts
+++ src/types/settingsTypes.ts
@@ -1,11 +1,3 @@
-type WatchOptions = {
-    active: boolean,
-    watch: {
-        windowName: string,
-        command: string,
-    }
-}
-
 type Autosave = {
     active: boolean,
     currentSession: string,
